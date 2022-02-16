39a7c1a0b621e950a556ae4c50ce14926d613435
Hasan ER <hasanfatiher@gmail.com>
2022-02-16 02:03:34 -0500

    vs1

---
+++ src/test/java/TestNGProjeler/BasicAuthentications.java
@@ -0,0 +1,12 @@
+package TestNGProjeler;
+/* BasicAuthentications iki sunucu arası veri paylaşımı için sadece o kişiye verilen
+* erişim iznini Authentications ile sağlanır.
+
+* BasicAuthentications bir HTML komutudur. Uygulamaya, sisteme kimin girip kimin giremeyeceğini
+belirler.
+
+* Servis sağlayıcı size özel username ve password'u (giriş url'si) size vermelidir.
+
+*/
+public class BasicAuthentications {
+}
