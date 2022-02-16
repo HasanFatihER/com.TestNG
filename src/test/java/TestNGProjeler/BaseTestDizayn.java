974a1de944eeff5d3e1094a644087c254582d848
Hasan ER <hasanfatiher@gmail.com>
2022-02-16 07:44:39 -0500

    TestBase

---
+++ src/test/java/TestNGProjeler/BaseTestDizayn.java
@@ -0,0 +1,20 @@
+package TestNGProjeler;
+
+import Utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class BaseTestDizayn extends TestBase {
+    /* testNG'nin özelliği olarak @Test kodunu gördüğünde öncelikle @Before... koduna bakar ve
+    * ilk onu çalıştırmak ister.. Bu class'ta olmadığı için parent'a gider. Test bittikten sonra da
+    * @After'ı arar. O da olmadığı için yine parent'a gider.
+    *
+    * Bir teknik olarak @Before ve @After methodları için de bir parent oluşturabiliriz. Hangi
+    * yöntem test için uygun olursa onu extends ederiz.
+    * */
+
+    @Test
+    public void baseTest(){
+        driver.get("https://www.amazon.com/");
+    }
+
+}
